7e0fc31dcd6ed25f212037a1826aa92b9a233e8c
Kathe01 <kathe.fernandez0104@gmail.com>
2019-03-27 12:09:04 -0500

    Aporte de estudiante  Reloj

---
+++ src/ec/edu/insuperior/controlador/controlador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.insuperior.controlador;
+
+import ec.edu.insuperior.vista.Principal;
+/**
+ *
+ * @author PC-IVAN
+ */
+public class controlador {
+    
+    public static void main(String[] args) {
+        Principal a=new Principal();
+        a.setVisible(true);
+    }
+           
+ }
+        
+        
+        
+    
+
